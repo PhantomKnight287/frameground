7beae71736bd2064641c90b9a598b1a8716bddae
Gurpal Singh <gillgurpal699@gmail.com>
2024-01-21 18:46:26 +0530

    [docs] ignore build errors

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,10 +1,11 @@
-import createNextDocsMDX from 'next-docs-mdx/config';
+import createNextDocsMDX from "next-docs-mdx/config";
 
 const withMDX = createNextDocsMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default withMDX(config);
