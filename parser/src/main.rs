ec748193225b9d8523e3e26210a02e87d2640fe4
Gurpal Singh <gillgurpal699@gmail.com>
2024-01-21 18:33:18 +0530

    [parser] convert into proper cli

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,7 +1,48 @@
+use clap::Parser;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs;
 use std::path::Path;
-use serde::{Serialize, Deserialize};
+
+#[allow(non_camel_case_types)]
+#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
+enum Status {
+    active,
+    #[default]
+    coming_soon,
+}
+
+#[derive(Parser, Debug)]
+#[clap(
+    version = "1.0",
+    author = "PhantomKnight287",
+    about = "A command line tool to create tracks for FrameGround or parse folders to json files."
+)]
+enum Command {
+    #[command(name = "create-track", about = "Create a track for FrameGround")]
+    CreateTrack {
+        #[arg(required = true, short = 'n', long = "name")]
+        name: String,
+
+        #[arg(required = true, short = 'd', long = "description")]
+        description: String,
+
+        #[arg(required = true, short = 's', long = "slug")]
+        slug: String,
+
+        #[arg(required = true, short = 'l', long = "logo")]
+        logo: String,
+
+        #[arg(required = true, short = 't', long = "status")]
+        status: Status,
+    },
+
+    #[command(name = "parse", about = "Parse folder into json files")]
+    Parse {
+        #[arg(required = true, short = 'f', long = "folder")]
+        folder: String,
+    },
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
@@ -45,24 +86,66 @@ fn read_folder(path: &Path) -> Vec<Item> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let command = Command::parse();
+    match command {
+        Command::CreateTrack {
+            name,
+            description,
+            slug,
+            logo,
+            status,
+        } => {
+            println!(
+                "Creating track with name: {}, description: {}, slug: {}, logo: {}, status: {:?}",
+                name, description, slug, logo, status
+            );
+            // look for challenges folder outside of the parser folder, relative to the current working directory
+            let mut challenges_path = env::current_dir().unwrap();
+            challenges_path.pop();
+            challenges_path.push("challenges");
+            // check if challenges folder exists
+            if !challenges_path.is_dir() {
+                eprintln!("Error: {} is not a directory.", challenges_path.display());
+                std::process::exit(1);
+            }
+            // create track folder
+            let mut track_path = challenges_path.clone();
+            track_path.push(slug.clone());
+            fs::create_dir(&track_path).expect("Failed to create track folder");
+            // create track.json
+            let mut track_json_path = track_path.clone();
+            track_json_path.push("track.json");
+            let track_json = format!(
+                r#"{{
+    "$schema":"../track.schema.json",
+    "name": "{}",
+    "description": "{}",
+    "slug": "{}",
+    "logo": "{}",
+    "status": "{:#?}"
+}}"#,
+                name, description, slug, logo, status
+            );
+            // override track.json if it already exists
+            if track_json_path.is_file() {
+                fs::remove_file(&track_json_path).expect("Failed to remove track.json");
+            }
+            fs::write(&track_json_path, track_json).expect("Failed to create track.json");
+        }
 
-    if args.len() != 2 {
-        eprintln!("Usage: {} <folder_path>", args[0]);
-        std::process::exit(1);
+        Command::Parse { folder } => {
+            let folder_path = Path::new(folder.as_str());
+
+            if !folder_path.is_dir() {
+                eprintln!("Error: {} is not a directory.", folder_path.display());
+                std::process::exit(1);
+            }
+
+            let result = read_folder(folder_path);
+
+            // Serialize and write to files.json without the root key
+            let json_result = serde_json::to_string_pretty(&result).unwrap();
+            fs::write("files.json", json_result).expect("Failed to write to files.json");
+        }
     }
-
-    let folder_path = &args[1];
-    let folder_path = Path::new(folder_path);
-
-    if !folder_path.is_dir() {
-        eprintln!("Error: {} is not a directory.", folder_path.display());
-        std::process::exit(1);
-    }
-
-    let result = read_folder(folder_path);
-
-    // Serialize and write to files.json without the root key
-    let json_result = serde_json::to_string_pretty(&result).unwrap();
-    fs::write("files.json", json_result).expect("Failed to write to files.json");
 }
