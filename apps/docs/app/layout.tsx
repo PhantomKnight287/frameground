d0d0df3e77f807b1297023090b5ebcab55fb6d78
Gurpal Singh <gillgurpal699@gmail.com>
2024-01-23 13:24:18 +0530

    [docs] fix routes

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,17 +1,29 @@
-import './global.css';
-import { RootProvider } from 'next-docs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import { DocsLayout } from "next-docs-ui/layout";
+import "./global.css";
+import { RootProvider } from "next-docs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import { pageTree } from "./source";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <DocsLayout
+            tree={pageTree}
+            nav={{
+              title: "FrameGround",
+              githubUrl: "https://github.com/phantomknight287/frameground",
+            }}
+          >
+            {children}
+          </DocsLayout>
+        </RootProvider>
       </body>
     </html>
   );
--- apps/docs/app/source.ts
+++ apps/docs/app/source.ts
@@ -3,7 +3,7 @@ import { createMDXSource } from 'next-docs-mdx';
 import { loader } from 'next-docs-zeta/source';
 
 export const { getPage, getPages, pageTree } = loader({
-  baseUrl: '/docs',
-  rootDir: 'docs',
+  baseUrl: '/',
+  rootDir: '',
   source: createMDXSource(map),
 });
