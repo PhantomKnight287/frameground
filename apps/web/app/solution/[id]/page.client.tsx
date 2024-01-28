fa677b666ed078b6412ef255da29eb7ab7f64048
Gurpal Singh <gillgurpal699@gmail.com>
2024-01-28 23:36:15 +0530

    [web] create page to show solution

---
+++ apps/web/app/solution/[id]/page.client.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { languages } from "@/constants/languages";
+import { File } from "@repo/db/types";
+import MonacoEditor from "@repo/monaco";
+import { useState } from "react";
+
+function SolutionFiles({ files }: { files: File[] }) {
+  const [activeFile, setActiveFile] = useState(`${files[0].name}-0`);
+  return (
+    <div className="flex flex-col items-start mt-8">
+      <div className="flex flex-row no-scrollbar gap-2 flex-nowrap">
+        {files.map((file, index) => (
+          <Button
+            key={file.id}
+            onClick={() => setActiveFile(`${file.name}-${index}`)}
+            className="rounded-b-none"
+          >
+            {file.name}
+          </Button>
+        ))}
+      </div>
+      <MonacoEditor
+        activeFile={activeFile}
+        options={{ readOnly: true }}
+        value={
+          files.find((file, index) => `${file.name}-${index}` === activeFile)
+            ?.content!
+        }
+        height={"80vh"}
+        theme="vs-dark"
+        language={
+          // @ts-expect-error
+          languages[
+            files
+              .find((file, index) => `${file.name}-${index}` === activeFile)
+              ?.name.split(".")[1] as any
+          ]
+        }
+      />
+    </div>
+  );
+}
+
+export default SolutionFiles;
---
+++ apps/web/app/solution/[id]/page.tsx
@@ -0,0 +1,80 @@
+import { prisma } from "@repo/db";
+import { PageProps } from "../../solutions/[id]/$types";
+import { notFound } from "next/navigation";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import SolutionFiles from "./page.client";
+import { Markdown } from "@/components/markdown";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+async function Solution({ params }: PageProps) {
+  const solution = await prisma.solution.findFirst({
+    where: {
+      id: params.id,
+    },
+    include: {
+      author: {
+        select: {
+          username: true,
+          name: true,
+        },
+      },
+      files: true,
+      challenge: {
+        select: {
+          slug: true,
+          track: {
+            select: {
+              slug: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  if (!solution) notFound();
+  return (
+    <div className="container mt-10 gap-4">
+      <div className="flex flex-row items-center gap-2 justify-between">
+        <h1 className="text-xl font-bold">{solution.title}</h1>
+        <Link
+          href={`/tracks/${solution.challenge.track!.slug}/challenge/${
+            solution.challenge.slug
+          }`}
+          className={buttonVariants({
+            variant: "secondary",
+          })}
+        >
+          Go to Challenge
+        </Link>
+      </div>
+      <div className="flex flex-row items-center justify-start w-fit my-4">
+        <Avatar>
+          <AvatarImage src={`/github-avatar/${solution.author.username}`} />
+          <AvatarFallback>{solution.author.username}</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col items-start justify-between pl-2">
+          {solution.author.name}
+          <Link
+            href={`/@${solution.author.username}`}
+            className={buttonVariants({
+              variant: "link",
+              className: "!p-0 h-fit !text-muted-foreground",
+            })}
+          >
+            @{solution.author.username}
+          </Link>
+        </div>
+      </div>
+
+      <Markdown
+        // eslint-disable-next-line react/no-children-prop
+        children={solution.description}
+      />
+
+      <SolutionFiles files={solution.files} />
+    </div>
+  );
+}
+
+export default Solution;
