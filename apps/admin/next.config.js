ec60eb9d52e5c5b37c9d72c9936eed1a4d3d22bc
Gurpal Singh <gillgurpal699@gmail.com>
2024-01-30 15:59:10 +0530

    add prisma webpack plugin

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,4 +1,12 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,16 @@
 import "./env.mjs";
 import { withNextJSRouteTypes } from "nextjs-route-types";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   async headers() {
     return [
       {
