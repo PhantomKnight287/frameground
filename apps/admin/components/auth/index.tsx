c85e784d63e238e1cf4af50c09fd5cac10f4099a
Gurpal Singh <gillgurpal699@gmail.com>
2024-01-30 15:43:41 +0530

    Add useEffect hook to VerifyUser component

--- apps/admin/components/auth/index.tsx
+++ apps/admin/components/auth/index.tsx
@@ -2,14 +2,17 @@
 import { assertAdmin } from "@/utils/auth";
 import { useSession } from "next-auth/react";
 import { redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function VerifyUser() {
   const { data } = useSession();
   const { replace } = useRouter();
-  if (assertAdmin(data)) return null;
-  else {
-    replace("/no-access");
-  }
+  useEffect(() => {
+    if (assertAdmin(data)) return void null;
+    else {
+      replace("/no-access");
+    }
+  }, [data]);
 }
 
 export default VerifyUser;
