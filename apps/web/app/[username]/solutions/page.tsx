9a2384d59a18c4f1218b74ed2aea21c33f14ab41
Gurpal Singh <gillgurpal699@gmail.com>
2024-03-05 23:29:18 +0530

    [web] create route for solutions

--- apps/web/app/[username]/solutions/page.tsx
+++ apps/web/app/[username]/solutions/page.tsx
@@ -1,4 +1,4 @@
-import { getCachedEnrolledTracks } from "@/cache/user";
+import { getCachedSolutionsPosted } from "@/cache/solutions";
 import {
   Table,
   TableBody,
@@ -7,45 +7,48 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { fromNow } from "@/utils/time";
 import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Enrolled Tracks",
+  title: "Solutions Posted",
 };
 
-async function Tracks({ params }: { params: { username: string } }) {
+async function SolutionsPosted({ params }: { params: { username: string } }) {
   const username = decodeURIComponent(params.username).replace("@", "");
-  const tracks = await getCachedEnrolledTracks(username);
+
+  const solutions = await getCachedSolutionsPosted(username);
+
   return (
     <>
       <div className="flex flex-col p-6 pb-0">
         <h3 className="text-lg font-semibold leading-none tracking-tight">
-          Enrolled Tracks
+          Solutions Posted
         </h3>
       </div>
       <div className="p-6">
-        {tracks.length > 0 ? (
+        {solutions.length > 0 ? (
           <Table>
-            <TableHeader>
+            <TableHeader className="not-in-markdown">
               <TableRow className="not-in-markdown">
                 <TableHead className="not-in-markdown">Name</TableHead>
-                <TableHead className="not-in-markdown">Description</TableHead>
+                <TableHead className="not-in-markdown">Created</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {tracks.map((track) => (
-                <TableRow key={track.id} className="not-in-markdown">
+              {solutions.map((solution) => (
+                <TableRow key={solution.id} className="not-in-markdown">
                   <TableCell className="font-medium not-in-markdown">
                     <Link
-                      href={`/track/${track.slug}`}
+                      href={`/solutions/${solution.id}`}
                       className="hover:underline"
                     >
-                      {track.name}
+                      <p className="line-clamp-1">{solution.title}</p>
                     </Link>
                   </TableCell>
                   <TableCell className="not-in-markdown">
-                    <p className="line-clamp-1">{track.description}</p>
+                    {fromNow(solution.createdAt)}
                   </TableCell>
                 </TableRow>
               ))}
@@ -57,4 +60,4 @@ async function Tracks({ params }: { params: { username: string } }) {
   );
 }
 
-export default Tracks;
+export default SolutionsPosted;
