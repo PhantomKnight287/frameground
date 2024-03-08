ea9bdf4a6a748d367383daa2724d92230cd94f4f
Gurpal Singh <gillgurpal699@gmail.com>
2024-03-08 22:37:42 +0530

    [web] add cache for challenges

---
+++ apps/web/cache/challenge.ts
@@ -0,0 +1,15 @@
+import {memoize} from "nextjs-better-unstable-cache";
+import {prisma} from "@/lib/db";
+
+export const getAllChallenges = memoize(async(trackSlug?:string|null)=>await prisma.challenge.findMany({
+    select:{
+        slug:true,label:true,
+    },
+    where: trackSlug ? {
+        track:{
+            slug:trackSlug
+        }
+    } : undefined
+}),{
+    revalidateTags:(trackSlug)=>[`challenges::${trackSlug||undefined}`]
+})
