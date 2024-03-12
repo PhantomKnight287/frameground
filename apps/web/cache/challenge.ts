8990b03bdad6b775a1d5bc9d778a7e4116ce0b59
Gurpal Singh <gillgurpal699@gmail.com>
2024-03-12 15:58:09 +0530

    [web] format

--- apps/web/cache/challenge.ts
+++ apps/web/cache/challenge.ts
@@ -1,15 +1,22 @@
-import {memoize} from "nextjs-better-unstable-cache";
-import {prisma} from "@/lib/db";
+import { memoize } from "nextjs-better-unstable-cache";
+import { prisma } from "@/lib/db";
 
-export const getAllChallenges = memoize(async(trackSlug?:string|null)=>await prisma.challenge.findMany({
-    select:{
-        slug:true,label:true,
-    },
-    where: trackSlug ? {
-        track:{
-            slug:trackSlug
-        }
-    } : undefined
-}),{
-    revalidateTags:(trackSlug)=>[`challenges::${trackSlug||undefined}`]
-})
+export const getAllChallenges = memoize(
+  async (trackSlug?: string | null) =>
+    await prisma.challenge.findMany({
+      select: {
+        slug: true,
+        label: true,
+      },
+      where: trackSlug
+        ? {
+            track: {
+              slug: trackSlug,
+            },
+          }
+        : undefined,
+    }),
+  {
+    revalidateTags: (trackSlug) => [`challenges::${trackSlug || undefined}`],
+  }
+);
