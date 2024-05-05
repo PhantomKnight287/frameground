6e340e032e6c77ec6c5c4db1d726038b36e2585f
Gurpal Singh <gillgurpal699@gmail.com>
2024-05-05 18:17:25 +0530

    [web] add FAQ modal for first time users

--- apps/web/app/tracks/[track]/challenge/[challenge]/page.client.tsx
+++ apps/web/app/tracks/[track]/challenge/[challenge]/page.client.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { Challenge, Upvote, User } from "@repo/db/types";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import ChallengeDescription from "@/components/challenge/description";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import FilterButton from "./solved/_components/filter-buttons";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function ChallengeTabs({
   challenge,
@@ -29,6 +37,16 @@ function ChallengeTabs({
   const { replace } = useRouter();
   const pathName = usePathname();
   const search = useSearchParams();
+  const [isFAQModalOpened, setIsFAQModalOpened] = useState(false);
+
+  useEffect(() => {
+    const value = localStorage.getItem("faq_modal_shown");
+    console.log(value);
+    if (!value || value === "false") {
+      setIsFAQModalOpened(true);
+    }
+  }, []);
+
   return (
     <Tabs defaultValue={queryParams?.tab || "description"}>
       <TabsList className="w-full">
@@ -74,6 +92,34 @@ function ChallengeTabs({
       </TabsList>
       <TabsContent value={"description"}>
         <ChallengeDescription challenge={challenge} params={params} />
+        <Dialog
+          open={isFAQModalOpened}
+          onOpenChange={(d) => {
+            localStorage.setItem("faq_modal_shown", String(!d));
+            setIsFAQModalOpened(false);
+          }}
+        >
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-gray-400">
+                Read This Before You Begin
+              </DialogTitle>
+              <p className="text-base text-white tracking-tight">
+                If you are first time visitor, running webcontainers require
+                extra browser config. Please read{" "}
+                <a
+                  href={`${process.env.NODE_ENV === "development" ? "http://localhost:3002" : "https://docs.frameground.tech"}/faq#playground-isnt-working-in-my-browser`}
+                  className="text-blue-500"
+                  target="_blank"
+                >
+                  FAQ
+                </a>{" "}
+                before you begin or if you are not getting any output in
+                terminal.
+              </p>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </TabsContent>
       <TabsContent value="comments" className="mt-0 h-screen">
         <div className="flex flex-row items-start justify-start gap-4 p-2 mt-2  px-0 bg-border h-screen">
